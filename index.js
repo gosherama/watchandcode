d24ac0c76bc31fe22fc09b1d00a111bea67c71c0
Gosherama <macciak@gmail.com>
2018-12-05 19:16:29 +0100

    Todo functions conversion to Object

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
-var todos = ['item 1', 'item 2', 'item 3', 'item 4'];
-
-// Display todos
-function displayTodos() {
-    console.log('My Todos:', todos);
-}
-
-// Add new todos
-function addTodo(todo) {
-    // Add todo item at the end of the todos array
-    todos.push(todo);
-    displayTodos();
-}
-
-// Change todo
-function changeTodo(position,newValue) {
-    todos[position] = newValue;
-    displayTodos();
-}
-
-// Delete todo
-function deleteTodo(position) {
-    todos.splice(position,1);
-    displayTodos();
-}
-
-addTodo('item 0')
-//changeTodo(0,'changed again')
-//deleteTodo(1)
+var todolist = {
+    todos: ['item 1', 'item 2', 'item 3', 'item 4'],
+    displayTodos: function() {
+        console.log('My todos:', this.todos);
+    },
+    addTodo: function(todo) {
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+    changeTodo: function(position, newValue) {
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) {
+        this.todos.splice(position,1);
+        this.displayTodos();
+    }
+};
 
 
 
