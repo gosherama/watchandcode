cdd4f86bcec61f8262d8ed15b6edc01a8f6b22a7
Gosherama <macciak@gmail.com>
2018-12-06 18:35:41 +0100

    dispayTodos conditional display

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var todolist = {
     todos: [],
     displayTodos: function() {
-        console.log('My todos:', this.todos);
+        if (this.todos.length === 0) {
+            console.log('Your todo list is empty !');
+        } else {
+            console.log('My todos:');
+            for (var i = 0; i < this.todos.length ; i++) {
+                if (this.todos[i].completed === true) {
+                    console.log('(x)', this.todos[i].todoText);
+                } else {
+                    console.log('()', this.todos[i].todoText);
+                }
+            }
+        }
     },
     addTodo: function(todoText) {
         this.todos.push({
